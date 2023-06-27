a79e4d9b2480d28c2b0c519ef6711e6f31f58a4a
Alcir Junior <junior@cajucomunica.com.br>
2023-06-27 11:08:20 -0300

    fix: change type event

--- src/domain/customer/event/handler/SendEmailWhenCustomerSetAddressHandler.ts
+++ src/domain/customer/event/handler/SendEmailWhenCustomerSetAddressHandler.ts
@@ -1,8 +1,8 @@
 import { EventHandlerInterface } from "../../../@shared";
-import { CustomerCreatedEvent } from "../CustomerCreatedEvent";
+import { CustomerSetAddressEvent } from "../CustomerSetAddressEvent";
 
-export class SendEmailCustomerSetAddressHandler implements EventHandlerInterface<CustomerCreatedEvent> {
-  handle(event: CustomerCreatedEvent): void {
+export class SendEmailCustomerSetAddressHandler implements EventHandlerInterface<CustomerSetAddressEvent> {
+  handle(event: CustomerSetAddressEvent): void {
     console.log('Customer Set Address Event', event);
     console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.address.street}, ${event.eventData.address.number}, ${event.eventData.address.city}, ${event.eventData.address.state}, ${event.eventData.address.zipCode}`);
   }
